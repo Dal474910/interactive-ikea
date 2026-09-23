3dd90c20773c11b7591e06402bef9380a721520b
Dal474910 <Dal474910@utdallas.edu>
2026-09-23 17:53:44 +0000

    changed curser

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@
 let targetElement = document.querySelector("h2");
 targetElement.addEventListener("mouseover", function() {
     targetElement.innerText= "TIME";
+    targetElement.style.cursor="wait";
 });
 
 targetElement.addEventListener("mouseout", function() {
 targetElement.innerText = "IKEA"
+targetElement.style.cursor= "pointer";
 });
